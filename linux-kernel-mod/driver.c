b3dc0416a82d178a29f5df96f9f84220afc039d2
pouya sadeghi <pouyasadeghi2012@gmail.com>
2022-10-09 22:53:07 +0330

    add driver-source-file

---
+++ linux-kernel-mod/driver.c
@@ -0,0 +1,20 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/uaccess.h>
+#include <linux/fs.h>
+#include <linux/proc_fs.h>
+
+MODULE_AUTHOR("group#3 members");
+MODULE_DESCRIPTION("this module print group#3's member name");
+MODULE_LICENSE("GPL");
+
+static int __init printing_members(void) {
+    printk(KERN_INFO "Pouya Sadeghi, Ali Ataollahi, Ali hodaee");
+    return 0;
+}
+static void __exit exiting_module(void) {
+    printk(KERN_INFO "we are group#3");
+}
+
+module_init(printing_members);
+module_exit(exiting_module);
