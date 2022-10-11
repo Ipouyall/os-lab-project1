f3eac7c105f61fa34f03df6949a3d89cd5e2d705
pouya sadeghi <pouyasadeghi2012@gmail.com>
2022-10-11 22:01:55 +0330

    update user-prog

--- codes/xv6/prime_numbers.c
+++ codes/xv6/prime_numbers.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 // program to get two number as command line arguments and print prime number between them
 int main(int argc, char *argv[])
 {
     int i, j, flag, start, end, n1, n2;
-    n1 = atoi(argv[1]);
-    n2 = atoi(argv[2]);
+    sscanf(argv[1], "%d", &n1);
+    sscanf(argv[2], "%d", &n2);
     if (n1 > n2)
     {
         start = n2;
