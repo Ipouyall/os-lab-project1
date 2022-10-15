ec13864c8c67677ca6973a14a10c76507bb7eed6
aliataollahi <aliataollahi40@gmail.com>
2022-10-15 08:05:45 -0400

    add tab place

--- codes/xv6/prefix_predict.c
+++ codes/xv6/prefix_predict.c
@@ -2,21 +2,25 @@
 
 #include "types.h"
 #include "user.h"
-
-
-
-int command_num=0,size=0;
-char command[5][30];
+int command_num = 0, size = 0 ;
+char command[15][30];
 
 static char*
-stringcpy(char *s, const char *t)
+stringcpy(char *s, const char *t,int size)
 {
   char *os;
 
   os = s;
-  while((*s++ = *t++) != 0)
+  while(((*s++ = *t++) != 0) && size--)
     ;
   return os;
+}
+void updatehistory(char* inp,int size) {
+
+    stringcpy(command[command_num%15], inp , size);
+    command_num++;
+    if(size<15) size++;
+
 }
 
 
@@ -25,35 +29,28 @@ int startswith(char *pre, char *str)
    if(strncmp(str, pre, strlen(pre)) == 0) return 1;
    return 0;
 }
-void updatehistory(char inp[]) {
+char printcommand(char key[]){
 
-    stringcpy(command[command_num%5], inp);
-    command_num++;
-    if(size<5) size++;
-
-}
-void printcommand(char key[]){
-
-    if(size<5){
+    if(size<15){
         for (int i = 0; i < size; i++)
             if(startswith(key,command[i])) {
                 // printf(1,"%s",command[i]);
-                return;
+                return command[i];
             }
     }
     else {
-        int endIndex = ((command_num % 5) + 4) % 5 ;
-        int i = ( command_num % 5);
+        int endIndex = ((command_num % 15) + 4) % 15 ;
+        int i = ( command_num % 15);
         while (i != endIndex)
         {
             if(startswith(key,command[i])) {
                 // printf(1,"%s",command[i]);
-                return;
+                return command[i];
             }
             i++;
-            if(i==5) i = 0;
+            if(i==15) i = 0;
         }
-        // if(startswith(key,command[i])) printf(1,"%s",command[i]);
+        if(startswith(key,command[i])) command[i];
 
     }
 }
--- codes/xv6/prefix_predict.h
+++ codes/xv6/prefix_predict.h
@@ -1,6 +1,6 @@
-extern int command_num,size;
-extern char command[5][30];
+extern int command_num ,size ;
+extern char command[15][30];
 
 int startswith(char *pre, char *str);
-void updatehistory(char inp[]);
-void printcommand(char key[]);
+void updatehistory(char* inp,int size);
+char printcommand(char key[]);
