5f121a691c20d178e0e334e134eeb13146db92f6
aliataollahi <aliataollahi40@gmail.com>
2022-10-14 22:52:09 -0400

    change names v2

---
+++ codes/xv6/prefix_predict.c
@@ -0,0 +1,41 @@
+#include "prefix_predict.h"
+#include <cstring>
+
+int startswith(char *pre, char *str)
+{
+    size_t lenpre = strlen(pre),
+           lenstr = strlen(str);
+    return lenstr < lenpre ? 0 : memcmp(pre, str, lenpre) == 0;
+}
+void updatehistory(char inp[]) {
+
+    strcpy(command[command_num%5], inp);
+    command_num++;
+    if(size<5) size++;
+
+}
+void printcommand(char key[]){
+
+    if(size<5){
+        for (int i = 0; i < size; i++)
+            if(startswith(key,command[i])) {
+                printf("%s",command[i]);
+                return;
+            }
+    }
+    else {
+        int endIndex = ((command_num % 5) + 4) % 5 ;
+        int i = ( command_num % 5);
+        while (i != endIndex)
+        {
+            if(startswith(key,command[i])) {
+                printf("%s",command[i]);
+                return;
+            }
+            i++;
+            if(i==5) i = 0;
+        }
+        if(startswith(key,command[i])) printf("%s",command[i]);
+
+    }
+}
