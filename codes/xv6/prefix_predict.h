a74b3108de525df1fa770fd051537f6ae3693665
aliataollahi <aliataollahi40@gmail.com>
2022-10-15 10:44:31 -0400

    update history added

--- codes/xv6/prefix_predict.h
+++ codes/xv6/prefix_predict.h
@@ -1,6 +1,6 @@
-extern int command_num ,size ;
+extern int command_num ,sizeCommand ;
 extern char command[15][30];
 
 int startswith(char *pre, char *str);
-void updatehistory(char* inp,int size);
-char printcommand(char key[]);
+void updatehistory(char* inp,int sizeCommand);
+char* printcommand(char key[]);
