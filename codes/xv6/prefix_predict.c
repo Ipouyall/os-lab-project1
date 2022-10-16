5cde12a65f35f765246a3ae31ce886fb77bc42b4
aliataollahi <aliataollahi40@gmail.com>
2022-10-16 01:44:08 -0400

    refactor

--- codes/xv6/prefix_predict.c
+++ codes/xv6/prefix_predict.c
@@ -22,43 +22,8 @@ void updatehistory(char* inp,int sizeCommand) {
     if(sizeCommand<15) sizeCommand++;
 
 }
-
-
 int startswith(char *pre, char *str)
 {
    if(strncmp(str, pre, strlen(pre)) == 0) return 1;
    return 0;
-
-   /*
-       int len1=strlen(pre),len2=strlen(str);
-    if(len1>len2) return 0;
-    else
-        for (int i = 0; i < len1; i++)
-            if (pre[i]!=str[i])
-                return 0;
-   return 1;*/
-}
-char* printcommand(char key[]){
-    if(sizeCommand<15){
-        for (int i = 0; i < sizeCommand; i++)
-            if(startswith(key,command[i])) {
-                // printf(1,"%s",command[i]);
-                return command[i];
-            }
-    }
-    else {
-        int endIndex = ((command_num % 15) + 14) % 15 ;
-        int i = ( command_num % 15);
-        while (i != endIndex)
-        {
-            if(startswith(key,command[i])) {
-                // printf(1,"%s",command[i]);
-                return command[i];
-            }
-            i++;
-            if(i==15) i = 0;
-        }
-        if(startswith(key,command[i])) return command[i];
-
-    }
 }
--- codes/xv6/prefix_predict.h
+++ codes/xv6/prefix_predict.h
@@ -3,4 +3,3 @@ extern char command[15][30];
 
 int startswith(char *pre, char *str);
 void updatehistory(char* inp,int sizeCommand);
-char* printcommand(char key[]);
